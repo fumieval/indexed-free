616c67944364c9f4da3b8796023c624f0906b0a0
fumieval <fumiexcel@gmail.com>
2013-05-05 23:56:01 +0900

    free indexed monad transformer & version bump

--- Control/Monad/Indexed/Free.hs
+++ Control/Monad/Indexed/Free.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE GADTs, MultiParamTypeClasses, FlexibleInstances #-}
+{-# LANGUAGE GADTs, MultiParamTypeClasses, FlexibleInstances, Rank2Types #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Control.Monad.Indexed.Free
@@ -10,34 +10,34 @@
 -- Portability :  non-portable
 --
 ----------------------------------------------------------------------------
-module Control.Monad.Indexed.Free (IxFree(..)) where
+module Control.Monad.Indexed.Free (IxFree(..), hoistIxFree, module Control.Monad.Indexed.Free.Class) where
 
 import Control.Applicative
 import Control.Monad.Indexed
 import Control.Monad.Indexed.Free.Class
 
 data IxFree f i j x where
-    IxPure :: a -> IxFree f i i a
-    IxFree :: f i j (IxFree f j k a) -> IxFree f i k a
+    Pure :: a -> IxFree f i i a
+    Free :: f i j (IxFree f j k a) -> IxFree f i k a
 
 instance IxFunctor f => IxFunctor (IxFree f) where
-    imap f (IxPure a) = IxPure (f a)
-    imap f (IxFree w) = IxFree (imap (imap f) w)
+    imap f (Pure a) = Pure (f a)
+    imap f (Free w) = Free (imap (imap f) w)
 
 instance IxFunctor f => IxPointed (IxFree f) where
-    ireturn = IxPure
+    ireturn = Pure
 
 instance IxFunctor f => IxApplicative (IxFree f) where
-    iap (IxPure a) (IxPure b) = IxPure (a b)
-    iap (IxPure a) (IxFree fb) = IxFree (imap a `imap` fb)
-    iap (IxFree fa) mb = IxFree $ imap (`iap` mb) fa
+    iap (Pure a) (Pure b) = Pure (a b)
+    iap (Pure a) (Free fb) = Free (imap a `imap` fb)
+    iap (Free fa) mb = Free $ imap (`iap` mb) fa
 
 instance IxFunctor f => IxMonad (IxFree f) where
-    ibind k (IxPure a) = k a
-    ibind k (IxFree fm) = IxFree $ imap (ibind k) fm
+    ibind k (Pure a) = k a
+    ibind k (Free fm) = Free $ imap (ibind k) fm
 
 instance IxFunctor f => IxMonadFree f (IxFree f) where
-    iwrap = IxFree
+    iwrap = Free
 
 instance IxFunctor f => Functor (IxFree f i i) where
     fmap = imap
@@ -49,3 +49,7 @@ instance IxFunctor f => Applicative (IxFree f i i) where
 instance IxFunctor f => Monad (IxFree f i i) where
     return = ireturn
     (>>=) = (>>>=)
+
+hoistIxFree :: (IxFunctor f, IxMonadFree g m) => (forall i j x. f i j x -> g i j x) -> IxFree f i j a -> m i j a
+hoistIxFree _ (Pure a) = ireturn a
+hoistIxFree f (Free fm) = iwrap $ f (imap (hoistIxFree f) fm)
---
+++ Control/Monad/Indexed/Trans/Free.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE GADTs, MultiParamTypeClasses, FlexibleInstances, Rank2Types #-}
+module Control.Monad.Indexed.Trans.Free (IxFreeF(..), module Control.Monad.Indexed.Free.Class) where
+
+import Control.Applicative
+import Control.Monad.Indexed
+import Control.Monad.Indexed.Free.Class
+
+data IxFreeF f m i j a where
+    Pure :: a -> IxFreeF f m i i a
+    Free :: f i j (IxFreeT f m j k a) -> IxFreeF f m i k a
+
+newtype IxFreeT f m i j a = IxFreeT { runIxFreeT :: m (IxFreeF f m i j a) }
+
+bindIxFreeT :: (IxFunctor f, Monad m) => IxFreeT f m i j a -> (a -> IxFreeT f m j k b) -> IxFreeT f m i k b
+bindIxFreeT (IxFreeT m) k = IxFreeT $ m >>= \r -> case r of
+    Pure a -> runIxFreeT (k a)
+    Free f -> return $ Free $ imap (`bindIxFreeT` k) f
+
+instance (Monad m, IxFunctor f) => IxFunctor (IxFreeT f m) where
+    imap f (IxFreeT m) = IxFreeT $ m >>= \r -> case r of
+        Pure a -> return $ Pure (f a)
+        Free fm -> return $ Free $ imap (imap f) fm
+
+instance (Monad m, IxFunctor f) => IxPointed (IxFreeT f m) where
+    ireturn = IxFreeT . return . Pure
+
+instance (Monad m, IxFunctor f) => IxApplicative (IxFreeT f m) where
+    iap mf m = bindIxFreeT mf $ \f -> bindIxFreeT m $ \a -> return (f a)
+
+instance (Monad m, IxFunctor f) => IxMonad (IxFreeT f m) where
+    ibind = flip bindIxFreeT
+
+instance (Monad m, IxFunctor f) => IxMonadFree f (IxFreeT f m) where
+    iwrap = IxFreeT . return . Free
+
+instance (Monad m, IxFunctor f) => Functor (IxFreeT f m i i) where
+    fmap = imap
+
+instance (Monad m, IxFunctor f) => Applicative (IxFreeT f m i i) where
+    pure = ireturn
+    (<*>) = iap
+
+instance (Monad m, IxFunctor f) => Monad (IxFreeT f m i i) where
+    return = ireturn
+    (>>=) = (>>>=)
